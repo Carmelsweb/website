a2364bb5529accbbc254aacd8ece9426542b3214
Your Nam <you@example.com>
2025-10-05 20:44:10 +0100

    working currently

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import WestCoastCelebrantsModern from './Website.tsx'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <WestCoastCelebrantsModern />
+  </StrictMode>
 )
